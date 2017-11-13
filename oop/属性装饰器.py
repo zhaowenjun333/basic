937fb5f5152474da07654525f0433c6f2db61097
zhaowenjun333 <zhaoyun4240@163.com>
2017-11-13 20:30:27 +0800

    对象处理9

--- oop/属性装饰器.py
+++ oop/属性装饰器.py
@@ -4,21 +4,22 @@ class Person:
         self._chinese=chinese
         self._eng=eng
         self._history=history
-    @property
-    def getchinese(self):
+    @property   #  属性装饰器       ,只有私有属性才会有这个getter setter 方法
+    def chinese(self):
         return self._chinese
-    @getchinese.setter
-    def setchinese(self,chinese):
+    @chinese.setter
+    def chinese(self,chinese):
         self._chinese=chinese
-    @getchinese.deleter
-    def delechinese(self):
+    @chinese.deleter
+    def chinese(self):
         del self._chinese
     def __del__(self):
         print("game over")
 
 p1 =Person(67,89,45)
-print(p1.getchinese)
+print(p1.chinese)
 p1.setchinese=999
-print(p1.getchinese)
-p1.delechinese
-print(p1.getchinese)
+print(p1.chinese)
+del p1.chinese
+# print(p1.chinese)
+
--- oop/装饰一个类.py
+++ oop/装饰一个类.py
@@ -1,10 +1,23 @@
-def setnameproperty(name):
+"""利用装饰器给  类增加方法和属性
+
+强烈推荐这样的方法实现
+
+好处管理起来非常便利,插件一样 一行管理 ,开关的形式
+"""
+
+
+def property_setnameproperty(name):
     def wrapp(cls):
         cls.NAME = name
         return cls
     return wrapp
+
+def methond_printable(cls):
+    cls.print=lambda self:self.content
+    return cls
 # Myclass=setnameproperty(name)(MyClass)
-@setnameproperty("this is my class name called zhaoyuncreated class ")
+@property_setnameproperty("this is my class name called zhaoyuncreated class ")
+@methond_printable
 class MyClass:
     pass
 
