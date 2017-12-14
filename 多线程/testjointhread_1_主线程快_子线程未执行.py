a2ccacc9e6f7a1487befae4fc3203af798c07d7e
zhaowenjun333 <zhaoyun4240@163.com>
2017-12-14 13:59:36 +0800

    多线程1

---
+++ 多线程/testjointhread_1_主线程快_子线程未执行.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.6
+# coding: utf-8
+#testjointhread.PY
+# Created on 2017/12/13
+# @author: zhaoyun
+"""
+description:
+
+"""
+import time,threading
+class A:
+    def showme(self):
+        time.sleep(1)
+        print("lsllsl")
+
+
+if __name__=="__main__":
+    t1=threading.Thread(target=A().showme,daemon=True)# 主线程不会等待子线程
+    t1.start()
+    # time.sleep(2)
+    print("iam mai thread")
